73757b8356dbd327196755c0ca4d68c9d99849ca
Burley <db12797@gmail.com>
2018-06-20 10:51:36 -0500

    random button work and a random color is picked on page load.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const values = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
 const body = document.getElementById('background');
+const button = document.getElementById('button');
 
 const colorPicker = (e) => {
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
@@ -9,6 +10,23 @@ const colorPicker = (e) => {
   values.textContent = body.style.background + ";";
 }
 
-color1.addEventListener('input', colorPicker);
 
+
+const randColor = () => {
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+const setRandomColor = () => {
+  body.style.background = "linear-gradient(to right, " +
+	randColor() + ", " + randColor() + ")";
+	values.textContent = body.style.background + ";";
+}
+
+
+window.onload = setRandomColor();
+color1.addEventListener('input', colorPicker);
 color2.addEventListener('input', colorPicker);
+button.addEventListener('click', setRandomColor);
